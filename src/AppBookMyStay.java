563cf10549e7306be1f27c85e3ff723450982093
Shashank-Prasad7 <shashank.prasad.cr7@gmail.com>
2026-03-17 10:05:32 +0530

    UC1: Application entry and welcome message

---
+++ src/AppBookMyStay.java
@@ -0,0 +1,19 @@
+/**
+ * Hotel Booking Application Entry Point
+ * UC1 Implementation
+ *
+ * @author bloodybelly
+ * @version 1.0
+ */
+
+public class AppBookMyStay {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to BookMyStay ");
+        System.out.println(" Version 1.0 ");
+        System.out.println("=================================");
+
+    }
+}
---
+++ src/Room.java
@@ -0,0 +1,16 @@
+public abstract class Room {
+
+    protected String type;
+    protected int beds;
+    protected double price;
+
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: " + price);
+    }
+}
